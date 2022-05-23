4e2ff4736d5a8c23009c0886cc802dca602f32de
mahsa khodaei <mahsa.khodaei811@gmail.com>
2022-05-23 09:53:10 +0430

    changed stateless to statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:rock_paper_scissors/UI/definition/color_type.dart';
 
@@ -5,9 +7,16 @@ void main() {
   runApp(HomeScreen());
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int top = 2;
+  int bottom = 3;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,36 +35,42 @@ class HomeScreen extends StatelessWidget {
           centerTitle: true,
         ),
         backgroundColor: GameColors().GameGray,
-        body: _getBody(),
-      ),
-    );
-  }
-
-  Widget _getBody() {
-    return SafeArea(
-      child: Column(
-        children: [
-          SizedBox(width: double.infinity),
-          Expanded(
-            flex: 3,
-            child: Image(
-              image: AssetImage('images/1.png'),
-            ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(width: double.infinity),
+              Expanded(
+                flex: 3,
+                child: Image(
+                  image: AssetImage('images/$top.png'),
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  primary: GameColors().MainWhite,
+                  minimumSize: Size(150, 60),
+                  backgroundColor: GameColors().GameBlue,
+                ),
+                onPressed: () {
+                  setState(() {
+                    top = Random().nextInt(3) + 1;
+                    bottom = Random().nextInt(3) + 1;
+                  });
+                },
+                child: Text(
+                  'شروع بازی',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Image(
+                  image: AssetImage('images/$bottom.png'),
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            flex: 1,
-            child: TextButton(
-              onPressed: () {},
-              child: Text('بازی'),
-            ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Image(
-              image: AssetImage('images/2.png'),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
